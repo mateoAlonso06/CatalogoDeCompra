1a2531f6d8e94861f2af99ebf000a13be3bec5c9
mateoAlonso6 <alonsomateo2002@gmail.com>
2024-11-26 13:18:06 -0300

    Reestructuracion y renombre
    Se reorganizo la forma de anidamiento de los archivos principales
    y se renombro la solucion a una mas adecuada y con mas sentido
    para el proyecto

---
+++ presentacion/VentanaAgregar.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace presentacion
+{
+    partial class VentanaAgregar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelDescripcion = new System.Windows.Forms.Label();
+            this.labelMarca = new System.Windows.Forms.Label();
+            this.labelPrecio = new System.Windows.Forms.Label();
+            this.labelCategoria = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.listaMarca = new System.Windows.Forms.ComboBox();
+            this.listaCategoria = new System.Windows.Forms.ComboBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.imgAgregar = new System.Windows.Forms.PictureBox();
+            this.labelUrl = new System.Windows.Forms.Label();
+            this.labelCodArt = new System.Windows.Forms.Label();
+            this.txtCodigoArticulo = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.imgAgregar)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnAceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Location = new System.Drawing.Point(184, 379);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(117, 36);
+            this.btnAceptar.TabIndex = 15;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.BackColor = System.Drawing.Color.Transparent;
+            this.Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.ForeColor = System.Drawing.Color.Black;
+            this.Titulo.Location = new System.Drawing.Point(26, 10);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(266, 24);
+            this.Titulo.TabIndex = 14;
+            this.Titulo.Text = "Agregar artículo al catálogo";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.White;
+            this.btnCancelar.Location = new System.Drawing.Point(345, 380);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(130, 35);
+            this.btnCancelar.TabIndex = 17;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombre.ForeColor = System.Drawing.Color.Black;
+            this.labelNombre.Location = new System.Drawing.Point(22, 80);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(126, 17);
+            this.labelNombre.TabIndex = 0;
+            this.labelNombre.Text = "Nombre del artículo";
+            // 
+            // labelDescripcion
+            // 
+            this.labelDescripcion.AutoSize = true;
+            this.labelDescripcion.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDescripcion.ForeColor = System.Drawing.Color.Black;
+            this.labelDescripcion.Location = new System.Drawing.Point(312, 11);
+            this.labelDescripcion.Name = "labelDescripcion";
+            this.labelDescripcion.Size = new System.Drawing.Size(76, 17);
+            this.labelDescripcion.TabIndex = 1;
+            this.labelDescripcion.Text = "Descripción";
+            // 
+            // labelMarca
+            // 
+            this.labelMarca.AutoSize = true;
+            this.labelMarca.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMarca.ForeColor = System.Drawing.Color.Black;
+            this.labelMarca.Location = new System.Drawing.Point(22, 262);
+            this.labelMarca.Name = "labelMarca";
+            this.labelMarca.Size = new System.Drawing.Size(45, 17);
+            this.labelMarca.TabIndex = 2;
+            this.labelMarca.Text = "Marca";
+            // 
+            // labelPrecio
+            // 
+            this.labelPrecio.AutoSize = true;
+            this.labelPrecio.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrecio.ForeColor = System.Drawing.Color.Black;
+            this.labelPrecio.Location = new System.Drawing.Point(22, 144);
+            this.labelPrecio.Name = "labelPrecio";
+            this.labelPrecio.Size = new System.Drawing.Size(44, 17);
+            this.labelPrecio.TabIndex = 3;
+            this.labelPrecio.Text = "Precio";
+            // 
+            // labelCategoria
+            // 
+            this.labelCategoria.AutoSize = true;
+            this.labelCategoria.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCategoria.ForeColor = System.Drawing.Color.Black;
+            this.labelCategoria.Location = new System.Drawing.Point(25, 204);
+            this.labelCategoria.Name = "labelCategoria";
+            this.labelCategoria.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.labelCategoria.Size = new System.Drawing.Size(65, 17);
+            this.labelCategoria.TabIndex = 4;
+            this.labelCategoria.Text = "Categoría";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(25, 100);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(234, 21);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDescripcion.Location = new System.Drawing.Point(315, 34);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(234, 21);
+            this.txtDescripcion.TabIndex = 7;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrecio.Location = new System.Drawing.Point(25, 164);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(234, 21);
+            this.txtPrecio.TabIndex = 8;
+            // 
+            // listaMarca
+            // 
+            this.listaMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaMarca.FormattingEnabled = true;
+            this.listaMarca.Location = new System.Drawing.Point(25, 282);
+            this.listaMarca.Name = "listaMarca";
+            this.listaMarca.Size = new System.Drawing.Size(234, 23);
+            this.listaMarca.TabIndex = 9;
+            // 
+            // listaCategoria
+            // 
+            this.listaCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaCategoria.FormattingEnabled = true;
+            this.listaCategoria.Location = new System.Drawing.Point(25, 224);
+            this.listaCategoria.Name = "listaCategoria";
+            this.listaCategoria.Size = new System.Drawing.Size(234, 23);
+            this.listaCategoria.TabIndex = 10;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(315, 100);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(234, 21);
+            this.txtUrl.TabIndex = 11;
+            this.txtUrl.Leave += new System.EventHandler(this.txtUrl_Leave);
+            // 
+            // imgAgregar
+            // 
+            this.imgAgregar.Location = new System.Drawing.Point(343, 127);
+            this.imgAgregar.Name = "imgAgregar";
+            this.imgAgregar.Size = new System.Drawing.Size(174, 178);
+            this.imgAgregar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgAgregar.TabIndex = 12;
+            this.imgAgregar.TabStop = false;
+            // 
+            // labelUrl
+            // 
+            this.labelUrl.AutoSize = true;
+            this.labelUrl.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUrl.ForeColor = System.Drawing.Color.Black;
+            this.labelUrl.Location = new System.Drawing.Point(315, 80);
+            this.labelUrl.Name = "labelUrl";
+            this.labelUrl.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.labelUrl.Size = new System.Drawing.Size(105, 17);
+            this.labelUrl.TabIndex = 13;
+            this.labelUrl.Text = "Url de la imagen";
+            // 
+            // labelCodArt
+            // 
+            this.labelCodArt.AutoSize = true;
+            this.labelCodArt.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCodArt.ForeColor = System.Drawing.Color.Black;
+            this.labelCodArt.Location = new System.Drawing.Point(22, 11);
+            this.labelCodArt.Name = "labelCodArt";
+            this.labelCodArt.Size = new System.Drawing.Size(117, 17);
+            this.labelCodArt.TabIndex = 15;
+            this.labelCodArt.Text = "Codigo de artículo";
+            // 
+            // txtCodigoArticulo
+            // 
+            this.txtCodigoArticulo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCodigoArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoArticulo.Location = new System.Drawing.Point(25, 34);
+            this.txtCodigoArticulo.Name = "txtCodigoArticulo";
+            this.txtCodigoArticulo.Size = new System.Drawing.Size(234, 21);
+            this.txtCodigoArticulo.TabIndex = 16;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.txtCodigoArticulo);
+            this.panel1.Controls.Add(this.labelCodArt);
+            this.panel1.Controls.Add(this.labelUrl);
+            this.panel1.Controls.Add(this.imgAgregar);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Controls.Add(this.listaCategoria);
+            this.panel1.Controls.Add(this.listaMarca);
+            this.panel1.Controls.Add(this.txtPrecio);
+            this.panel1.Controls.Add(this.txtDescripcion);
+            this.panel1.Controls.Add(this.txtNombre);
+            this.panel1.Controls.Add(this.labelCategoria);
+            this.panel1.Controls.Add(this.labelPrecio);
+            this.panel1.Controls.Add(this.labelMarca);
+            this.panel1.Controls.Add(this.labelDescripcion);
+            this.panel1.Controls.Add(this.labelNombre);
+            this.panel1.Location = new System.Drawing.Point(30, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(583, 326);
+            this.panel1.TabIndex = 13;
+            // 
+            // VentanaAgregar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(623, 427);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAceptar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "VentanaAgregar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgAgregar)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelDescripcion;
+        private System.Windows.Forms.Label labelMarca;
+        private System.Windows.Forms.Label labelPrecio;
+        private System.Windows.Forms.Label labelCategoria;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.ComboBox listaMarca;
+        private System.Windows.Forms.ComboBox listaCategoria;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.PictureBox imgAgregar;
+        private System.Windows.Forms.Label labelUrl;
+        private System.Windows.Forms.Label labelCodArt;
+        private System.Windows.Forms.TextBox txtCodigoArticulo;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
